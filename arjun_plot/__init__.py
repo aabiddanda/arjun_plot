53f6b76158ec0b19aa164c9cb37f23fbe479e9ce
Arjun Biddanda <aabiddanda@gmail.com>
2020-10-24 12:34:21 -0500

    finalized some precommit hooks

--- arjun_plot/__init__.py
+++ arjun_plot/__init__.py
@@ -1,7 +1,9 @@
+"""Intialization for the arjun_plot library."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .utils import *
+# import utils as plot_utils
 
-__version__='0.1.0'
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
+"""Setup for the arjun_plot library."""
 
 from setuptools import setup
 
-version = '0.1.0'
+version = "0.1.0"
 
-required = open('requirements.txt').read().split('\n')
+required = open("requirements.txt").read().split("\n")
 
 setup(
-    name='arjun_plot',
+    name="arjun_plot",
     version=version,
-    description='custom matplotlib plotting routines',
-    author='aabiddanda',
-    author_email='aabiddanda@gmail.com',
-    url='https://github.com/aabiddanda/arjun_plot',
-    packages=['arjun_plot'],
+    description="custom matplotlib plotting routines",
+    author="aabiddanda",
+    author_email="aabiddanda@gmail.com",
+    url="https://github.com/aabiddanda/arjun_plot",
+    packages=["arjun_plot"],
     install_requires=required,
-    long_description='See ' + 'https://github.com/aabiddanda/arjun_plot',
-    license='MIT'
+    long_description="See " + "https://github.com/aabiddanda/arjun_plot",
+    license="MIT",
 )
