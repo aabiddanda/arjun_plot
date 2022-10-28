5b10a7a28fe763ed0d040e9f3d574b09dfec9f99
Arjun Biddanda <aabiddanda@gmail.com>
2022-10-28 11:38:08 -0700

    feat: added in all tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,3 +24,30 @@ def test_debox():
     assert ax.spines["top"].get_visible() is False
     assert ax.spines["bottom"].get_visible()
     assert ax.spines["left"].get_visible()
+
+
+def test_remove_border():
+    """Testing removal of the border entirely."""
+    _, ax = plt.subplots(1, 1)
+    utils.remove_border(ax)
+    for i in ax.spines:
+        assert ax.spines[i].get_visible() is False
+
+
+def test_remove_ticks():
+    """Testing removal of all ticks."""
+    _, ax = plt.subplots(1, 1)
+    utils.remove_ticks(ax)
+    assert ax.get_yticks().size == 0
+    assert ax.get_xticks().size == 0
+
+
+def test_label_multipanel():
+    """Test for labeling of multipanel plots."""
+    n = 3
+    labels = ["A", "B", "C"]
+    _, axs = plt.subplots(1, n)
+    utils.label_multipanel(axs, labels)
+    with pytest.raises(AssertionError):
+        _, axs = plt.subplots(1, 2)
+        utils.label_multipanel(axs, labels)
