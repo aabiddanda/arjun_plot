ff3fa7e5c5e66491685bc867978d7f18ecb51d60
Arjun Biddanda <aabiddanda@gmail.com>
2022-10-28 08:19:24 -0700

    added in some tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import pytest
+import matplotlib.pyplot as plt
 from arjun_plot import utils
 
 
@@ -7,3 +8,19 @@ def test_rand_jitter(test_in, test_seed, test_scale):
     """Testing random jitter."""
     jitter_res = utils.rand_jitter(test_in, scale=test_scale)
     assert test_in[0] != jitter_res[0]
+
+
+def test_plot_yx():
+    """Testing y=x plots."""
+    _, ax = plt.subplots(1, 1)
+    utils.plot_yx(ax)
+
+
+def test_debox():
+    """Testing of deboxing."""
+    _, ax = plt.subplots(1, 1)
+    utils.debox(ax)
+    assert ax.spines["right"].get_visible() is False
+    assert ax.spines["top"].get_visible() is False
+    assert ax.spines["bottom"].get_visible()
+    assert ax.spines["left"].get_visible()
