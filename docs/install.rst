c3fc92e1fdca2166d6a18d0ff21b70b3d0589864
Arjun Biddanda <aabiddanda@gmail.com>
2022-11-13 13:57:35 -0500

    docs: updated lots of documentation

--- docs/install.rst
+++ docs/install.rst
@@ -1,10 +1,20 @@
 Installation
 ============
 
-Installation of this package is quite simple via `pip` from source:
 
-```
-git clone https://github.com/aabiddanda/arjun_plot.git
-cd arjun_plot/
-pip install .
-```
+Installation from source 
+------------------------
+
+Currently, the only supported installation method is to install the package from source::
+  git clone https://github.com/aabiddanda/arjun_plot.git
+  cd arjun_plot/
+  pip install .
+
+
+
+Alternatively, you can use::
+
+  pip install git+https://github.com/user/repo.git@master
+
+We only recommend installation of the version on the main branch currently as this are the most stable versions.
+
---
+++ tests/test_pca.py
@@ -0,0 +1,42 @@
+import pytest
+import matplotlib.pyplot as plt
+from arjun_plot import pca
+
+def test_read_smartpca():
+    """Read a test smartpca output."""
+    pass
+
+
+def test_read_plinkpca():
+    """Read a test plink pca output."""
+    pass
+
+def test_add_poplabels():
+    """Testing adding some population labels. """
+    pass
+
+def test_add_meta_data():
+    """Testing adding in some new metadata."""
+    pass
+
+def test_check_data():
+    """Testing that intermediate data checks make sense."""
+    pass
+
+
+def test_calc_percent_variance():
+    """Testing that variance explained calculations make sense."""
+    pass
+
+
+def test_extract_medoid_pops_all():
+    """Test extraction of medoids per-population grouping."""
+    pass
+
+def test_pca_axis_labels():
+    """Test creation of PCA axis labels."""
+    pass
+
+def test_plot_pca():
+    """Test plotting of PCA."""
+    pass
