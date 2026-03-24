f1c203353dd2f88ef2590fe5baadeb9084acbddd
Arjun Biddanda <aabiddanda@gmail.com>
2026-03-24 10:55:28 -0400

    draft: ruff now works

--- arjun_plot/karyograms.py
+++ arjun_plot/karyograms.py
@@ -1,42 +1,51 @@
-import numpy as np
 import polars as pl
-from arjun_plot.utils import *
+import matplotlib.pyplot as plt
+from arjun_plot.utils import remove_border, remove_ticks
 
 
 def create_ideogram(chrom_df=None, scaling_factor=1e6, **kwargs):
-	"""
-
+    """
     :param polars.DataFrame chrom_df: Input axis.
     :param float scaling_factor: numpy array of p-values.
-
     Setup axes + rectangles for karyograms
-	"""
-	assert scaling_factor > 0
+    """
+    assert scaling_factor > 0
     if chrom_df is None:
-        raise ValueError('Chromosome lengths need to be specified')
+        raise ValueError("Chromosome lengths need to be specified")
     else:
         assert "chrom" in chrom_df.columns
         assert "size" in chrom_df.columns
         # NOTE: need to orient to specific chroms.
         fig, axs = plt.subplots(22, 1, **kwargs)
         m_size = 0
-        for i in range(1,23):
-            l = chrom_df.filter(pl.col('chrom') == f'chr{i}')['size'].to_numpy()[0]
-            if l >= m_size:
-                m_size = l
-            remove_border(axs[i-1]);
-            remove_ticks(axs[i-1]);
-            axs[i-1].add_patch(plt.Rectangle((0, 0), l/scaling_factor, 1, ls="-", lw=1, ec="black", fc="none"))
-            axs[i-1].plot([0,l/scaling_factor], [1,1], color='none')
-        return fig, axs, m_size
-
+        for i in range(1, 23):
+            chrom_len = chrom_df.filter(pl.col("chrom") == f"chr{i}")[
+                "size"
+            ].to_numpy()[0]
+            if chrom_len >= m_size:
+                m_size = chrom_len
+                remove_border(axs[i - 1])
+                remove_ticks(axs[i - 1])
+                axs[i - 1].add_patch(
+                    plt.Rectangle(
+                        (0, 0),
+                        chrom_len / scaling_factor,
+                        1,
+                        ls="-",
+                        lw=1,
+                        ec="black",
+                        fc="none",
+                    )
+                )
+                axs[i - 1].plot([0, chrom_len / scaling_factor], [1, 1], color="none")
+    return fig, axs, m_size
 
 
 def plot_tracts(features_df, axs, scaling_factor=1e6, n_features=3, **kwargs):
-	"""
+    """
     Plotting method for tracts
-	"""
-	for c in ["chrom", "start", "end"]:
-		assert c in features_df.columns
+    """
+    for c in ["chrom", "start", "end"]:
+        assert c in features_df.columns
     assert scaling_factor > 0
     pass
